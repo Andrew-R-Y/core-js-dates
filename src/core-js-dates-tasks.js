d75af702452c573f32cf68a8f62781457598fb94
Andrew-R-Y <rakitinandrei@gmail.com>
2024-03-25 22:03:33 +0300

    feat: add solution of fourth task

--- src/core-js-dates-tasks.js
+++ src/core-js-dates-tasks.js
@@ -74,8 +74,12 @@ function getDayName(date) {
  * Date('2024-02-13T00:00:00Z') => Date('2024-02-16T00:00:00Z')
  * Date('2024-02-16T00:00:00Z') => Date('2024-02-23T00:00:00Z')
  */
-function getNextFriday(/* date */) {
-  throw new Error('Not implemented');
+function getNextFriday(date) {
+  let myDate = new Date(new Date(date).getTime() + 24 * 3600 * 1000);
+  while (myDate.getDay() !== 5) {
+    myDate = new Date(new Date(myDate).getTime() + 24 * 3600 * 1000);
+  }
+  return myDate;
 }
 
 /**
